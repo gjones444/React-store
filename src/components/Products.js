b1af67d5650d65f94169fb883673d69971892633
Gjones444 <gjonesrei@gmail.com>
2019-05-27 02:06:02 -0400

    Final push

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,37 +1,52 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, {Component} from 'react';
+import Product from './Product';
+import LoadingProducts from '../loaders/Products';
+import NoResults from "../empty-states/NoResults";
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 
-class Product extends Component {
+class Products extends Component{
+	constructor(){
+		super();
+	}
+  	render(){
+    	let productsData;
+    	let term = this.props.searchTerm;
+    	let x;
 
-  constructor(props, context){
-    super(props,context)
-    this.state = {
-      products: []
-    };
-  }
+		function searchingFor(term){
+			return function(x){
+				return x.name.toLowerCase().includes(term.toLowerCase()) || !term;
+			}
+		}
+		productsData = this.props.productsList.filter(searchingFor(term)).map(product =>{
+			return(
+						<Product key={product.id} price={product.price} name={product.name} image={product.image} id={product.id} addToCart={this.props.addToCart} productQuantity={this.props.productQuantity} updateQuantity={this.props.updateQuantity} openModal={this.props.openModal}/>
+				)
+			}
+		);
 
-  getProducts(){
-      const url="https://quarkbackend.com/getfile/sivadass/products";
-  		axios.get(url)
-  	   .then(response => {
-          console.log(response)
-  				this.setState({
-  					products : response.data
-  				})
-  			})
-  	}
-
-    componentWillMount(){
-      this.getProducts();
-    }
-
-  render() {
-    return (
-      <div>
-        <p>Test anaofna fna fansf alknf aks fnaksnf anln</p>
-      </div>
-    );
-  }
+		// Empty and Loading States
+		let view;
+		if(productsData.length <= 0 && !term){
+			view = <LoadingProducts />
+		} else if(productsData.length <= 0 && term){
+			view = <NoResults />
+		} else{
+			view = <CSSTransitionGroup
+				transitionName="fadeIn"
+				transitionEnterTimeout={500}
+				transitionLeaveTimeout={300}
+				component="div"
+				className="products">
+					{productsData}
+			</CSSTransitionGroup>
+		}
+		return(
+			<div className="products-wrapper">
+				{view}
+			</div>
+		)
+	}
 }
 
-export default Product;
+export default Products;
