e61307d290f5688490aa57e2f491371b6c66d59d
Gjones444 <gjonesrei@gmail.com>
2019-05-27 02:14:17 -0400

    Final push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,9 @@ const Footer = (props) =>{
     return(
         <footer>
             <p className="footer-links">
-                <a href="https://github.com/sivadass/react-shopping-cart" target="_blank">View Source on Github</a>
+                <a href="https://github.com/gjones444/react-shopping-cart" target="_blank">View Source on Github</a>
                 <span>  /  </span>
-                <a href="mailto:contact@sivadass.in" target="_blank">Need any help?</a>
-                <span>  /  </span>
-                <a href="https://twitter.com/NSivadass" target="_blank">Say Hi on Twitter</a>
-                <span>  /  </span>
-                <a href="https://sivadass.in" target="_blank">Read My Blog</a>
+                <a href="mailto:gjonesrei@gmail.com" target="_blank">Need any help?</a>
             </p>
             <p>&copy; 2017 <strong>Veggy</strong> - Organic Green Store</p>
         </footer>
